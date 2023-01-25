a4c004a3f451b145b376cc3e5268c60660735d69
Frank Kelly <frankintech00@gmail.com>
2023-01-25 16:43:12 +0000

    added comments

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,20 +3,28 @@ import { AsyncPaginate } from "react-select-async-paginate";
 import { GEO_API_URL, geoApiOptions } from "../api";
 
 const Search = ({ onSearchChange }) => {
+  // useState hook to keep track of the current search input value
   const [search, setSearch] = useState(null);
 
+  // function to load options for the async paginate component
+  // inputValue is the current input value entered by the user
   const loadOptions = (inputValue) => {
+    // make a GET request to the specified API endpoint
+    // passing the inputValue as a query parameter
+    // and using the geoApiOptions for additional options
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=5000&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
       .then((response) => {
+        // format the response data and return it as options
         return {
           options: response.data.map((city) => {
             return {
               value: `${city.latitude}, ${city.longitude}`,
               label: `${city.name}, ${city.countryCode}`,
+              population: `${city.population}`,
             };
           }),
         };
@@ -24,11 +32,15 @@ const Search = ({ onSearchChange }) => {
       .catch((err) => console.error(err));
   };
 
+  // function to handle changes to the search input value
   const handleOnChange = (searchData) => {
+    console.log(searchData);
     setSearch(searchData);
     onSearchChange(searchData);
   };
 
+  // render the AsyncPaginate component
+  // passing the necessary props
   return (
     <AsyncPaginate
       placeholder="Search for a city..."
