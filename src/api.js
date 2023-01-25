807dfb96c92d8ae0dd35b663c9b9a9185dd51e47
Frank Kelly <frankintech00@gmail.com>
2023-01-25 14:32:25 +0000

    Added cities Api

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a855d71ee4msh540bc00eec5e092p1d121djsn02445aa3be9f",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import Search from "./Search";
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
   return (
-    <div className="App">
-      <Search />
+    <div className="h-screen bg-slate-500 flex justify-center items-center font-inter">
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
